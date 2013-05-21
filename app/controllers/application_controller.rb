b01645557f9ed80e0561012c1bd65f311cf64412
akadeev <a.kadeev@gmail.com>
2013-05-21 07:35:49 +0000

    AuthHelper added and authentication refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,7 @@
 class ApplicationController < ActionController::Base
+  include AuthHelper
+
   protect_from_forgery
 
-  before_filter :log_in_if_needed
-
-  private
-
-  def log_in_if_needed
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
-
-  def set_current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!set_current_user
-  end
-
-  helper_method :set_current_user
+  before_filter :authenticate_user!
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-  skip_before_filter :log_in_if_needed
+  skip_before_filter :authenticate_user!
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      sign_in(user)
       redirect_to root_url
     else
       render "login"
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out
     redirect_to root_url
   end
 end
