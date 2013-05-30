544e0bcf9d3d766cf7c114106eaaf8f2c3f05ccd
akadeev <a.kadeev@gmail.com>
2013-05-30 21:38:05 +0000

    Bugfix deployment on Heroku

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module TaskManager
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.initialize_on_precompile = false
     config.autoload_paths << "#{Rails.root}/lib"
   end
 end
