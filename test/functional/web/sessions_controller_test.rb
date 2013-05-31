3be351e457046e747f8c9deae1ad858524687907
akadeev <a.kadeev@gmail.com>
2013-05-31 12:00:28 +0000

    Fix tab indent

--- test/functional/web/sessions_controller_test.rb
+++ test/functional/web/sessions_controller_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
 class Web::SessionsControllerTest < ActionController::TestCase
-	setup do
-		@user = create :user
-	end
+  setup do
+    @user = create :user
+  end
   
-  	test "should get new" do
-    	get :new
-    	assert_response :success
-  	end
+    test "should get new" do
+      get :new
+      assert_response :success
+    end
 
-	test "should log in" do
-		attrs = {email: @user.email, password: "test"}
-		post :create, attrs
-		assert_response :redirect
-		assert signed_in?
-	end
+  test "should log in" do
+    attrs = {email: @user.email, password: "test"}
+    post :create, attrs
+    assert_response :redirect
+    assert signed_in?
+  end
 
-	test "should delete destroy" do
-		sign_in @user
-	    delete :destroy, id: @user.id
-	    assert_response :redirect
-	    assert !signed_in?
-	end
+  test "should delete destroy" do
+    sign_in @user
+      delete :destroy, id: @user.id
+      assert_response :redirect
+      assert !signed_in?
+  end
 end
